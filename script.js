a9ff2135204ffdc4ab34945391ebede6fb497ae0
amul patle <amulpatle3@gmail.com>
2023-05-31 00:35:54 +0530

    add location not found

--- script.js
+++ script.js
@@ -7,40 +7,52 @@ const description = document.querySelector(".description");
 const humidity = document.getElementById("humidity");
 const wind_speed = document.getElementById("wind-speed");
 
+const Location_not_found = document.querySelector(".location-not-found");
 
-async function checkWeather(city){
+const weather_body = document.querySelector(".weather-body");
+
+async function checkWeather(city) {
   const api_key = "d3dadd08143f0f4659d57312ccb45296";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
- const weather_data = await fetch(`${url}`).then(response =>response.json());
+  const weather_data = await fetch(`${url}`).then((response) =>
+    response.json()
+  );
 
-  temperature.innerHTML = `${Math.round(weather_data.main.temp -273.15)}°C`; 
+  if (weather_data.cod === `404`) {
+    Location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    console.log("error");
+    return;
+  }
+  Location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
+
+  temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
 
   description.innerHTML = `${weather_data.weather[0].description}`;
 
   humidity.innerHTML = `${weather_data.main.humidity}%`;
 
   wind_speed.innerHTML = `${weather_data.wind_speed}km/H`;
-  
-  switch(weather_data.weather[0].main)
-  {
-    case 'Clouds':
-     weather_img.src = "images/cloud.png";
-     break;
-    case 'clear':
-     weather_img.src = "images/clear.png";
-     break;
-    case 'Rain':
-     weather_img.src = "images/rain.png";
-     break;
-    case 'Mist':
-     weather_img.src = "images/mist.png";
-     break;
-    case 'Snow':
-     weather_img.src = "images/snow.png";
-     break;
-  }
 
+  switch (weather_data.weather[0].main) {
+    case "Clouds":
+      weather_img.src = "images/cloud.png";
+      break;
+    case "clear":
+      weather_img.src = "images/clear.png";
+      break;
+    case "Rain":
+      weather_img.src = "images/rain.png";
+      break;
+    case "Mist":
+      weather_img.src = "images/mist.png";
+      break;
+    case "Snow":
+      weather_img.src = "images/snow.png";
+      break;
+  }
 }
 
 searchBtn.addEventListener("click", () => {
